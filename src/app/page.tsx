3583292a6670037520de4f1f9c1d13a7fd388c36
Jayedjb765 <jayedjb76@gmail.com>
2025-07-29 23:51:52 +0600

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  console.log(blogs);
   return (
     <div className="my-10">
       <h1 className="text-4xl text-center">Latest Blogs</h1>
