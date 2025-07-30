1dc4f69be9717a2c448376106885daf141d9d2b2
Jayedjb765 <jayedjb76@gmail.com>
2025-07-30 19:47:43 +0600

    adding dynamic blogs

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import BlogCard from "@/components/ui/BlogCard";
 
-const BlogDetailsPage = () => {
-  return <div></div>;
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const res = await fetch(`http://localhost:3000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return (
+    <div>
+      <BlogCard blog={blog} />
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
