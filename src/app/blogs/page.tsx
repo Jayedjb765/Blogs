11b68696d4f6a84792666dc426560eb0f34bfbc4
Jayedjb765 <jayedjb76@gmail.com>
2025-08-01 23:46:55 +0600

    adding home

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,14 +1,23 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import Spinner from "@/components/ui/Spinner";
+import { useGetBlogsQuery } from "@/redux/apis/blogs.slice";
 import { Blog } from "@/types";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "NexaBlog | blogs",
-};
-const BlogsPage = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+
+// import { Blog } from "@/types";
+// import { Metadata } from "next";
+// export const metadata: Metadata = {
+//   title: "NexaBlog | blogs",
+// };
+const BlogsPage = () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store",
+  // });
+  // const blogs = await res.json();
+  const { data: blogs, isLoading } = useGetBlogsQuery({});
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <div className="w-[90%] mx-auto">
--- src/redux/apis/blogs.slice.ts
+++ src/redux/apis/blogs.slice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/blogs" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: (build) => ({
     getBlogs: build.query({
       query: () => `/blogs`,
@@ -13,4 +13,4 @@ export const baseApi = createApi({
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useLazyGetBlogsQuery } = baseApi;
+export const { useGetBlogsQuery } = baseApi;
